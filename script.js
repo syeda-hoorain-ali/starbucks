c90299e46d4e2553284198a2cc03692b88af2e95
Syeda Hoorain Ali <syedahoorainali8gmail.com>
2024-03-11 20:20:15 +0500

    initial commit

---
+++ script.js
@@ -0,0 +1,22 @@
+const navIcons = document.querySelector('.mobile-buttons');
+const navbar = document.querySelector('nav');
+
+
+
+navIcons.addEventListener('click', () => {
+    navIcons.classList.toggle('fa-bars', 'fa-xmark');
+    console.log(navIcons.classList);
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
